1ae50f0811497aac1aa6613b2bacc77f9d2730b7
Mike Lovely <mike-lovely@hotmail.co.uk>
2016-09-11 18:06:45 +0100

    display all surveys in index view

--- app/Http/Controllers/Admins/SurveyController.php
+++ app/Http/Controllers/Admins/SurveyController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Survey;
 
 class SurveyController extends Controller
 {
@@ -16,7 +17,9 @@ class SurveyController extends Controller
      */
     public function index()
     {
-        //
+        $surveys = Survey::all();
+        return view('admins.surveys.index')
+            ->with('surveys', $surveys);
     }
 
     /**
---
+++ resources/views/admins/surveys/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+		    <div class="col-md-12">
+		  		<a class="btn btn-info" href="{{ route('surveys.create') }}">Create new survey</a>
+		  	</div>
+		</div>
+		<hr>
+		<div class="row">
+		    <div class="col-md-12">
+		    	@foreach ($surveys as $survey)
+					<div class="panel panel-default">
+					  	<div class="panel-heading clearfix">
+					        <a class="btn btn-info" href="{{ route('surveys.show', [$survey->id]) }}">Show</a>
+					    	<a class="btn btn-info" href="{{ route('surveys.edit', [$survey->id]) }}">Edit</a>
+							<form action="{{ route('surveys.destroy', [$survey->id]) }}" class="pull-right" method="POST">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="_method" value="DELETE">
+								<button type="submit" class="btn btn-info">Delete</button>
+                            </form>
+					    </div>
+					  	<div class="panel-body">
+						    <h1>{{ $survey->title }}</h1>
+					        <p>{{ $survey->description }}</p>
+					        <a class="btn btn-small btn-info" href="{{ route('surveys.groups.index', [$survey->id]) }}">Manage</a>
+					  	</div>
+					  	<div class="panel-footer">
+					  		@if ($survey->anonymized == true)
+					  			<span class="label label-success">Anonymized</span>
+					  		@endif
+					  		@if ($survey->allow_registration == true)
+					  			<span class="label label-success">Allow registration</span>
+					  		@endif
+						    @if ($survey->active == true)
+						    	<span class="label label-success">Active</span>
+						    @endif
+					  	</div>
+					</div>
+				@endforeach
+	    	</div>
+		</div>
+    </div>
+@endsection
