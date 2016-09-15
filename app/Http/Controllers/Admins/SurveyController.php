89dd83bf2ab6908f433a6b168d5c65f0a2d952ac
Mike Lovely <mike-lovely@hotmail.co.uk>
2016-09-15 21:23:37 +0100

    use Request Form Object for structuring create survey form

--- app/Http/Controllers/Admins/SurveyController.php
+++ app/Http/Controllers/Admins/SurveyController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SurveyForm;
 use App\Models\Survey;
 
 class SurveyController extends Controller
@@ -39,9 +40,11 @@ class SurveyController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(SurveyForm $request)
     {
-        //
+        $request->persist();
+
+        return redirect()->route('surveys.index');
     }
 
     /**
---
+++ app/Http/Requests/SurveyForm.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Survey;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SurveyForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'slug' => 'required|alpha_dash|string|unique:surveys',
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'welcome_text' => 'required',
+            'end_text' => 'required',
+            'end_url' => 'url',
+            'admin_name' => 'required|string|max:100',
+            'admin_email' => 'required|email|max:255',
+            'allow_registration' => 'boolean',
+            'anonymized' => 'boolean',
+            'starts_at' => 'required|date',
+            'expires_at' => 'required|date',
+        ];
+    }
+
+    public function persist()
+    {
+        $post = Survey::create($this->all());
+    }
+}
