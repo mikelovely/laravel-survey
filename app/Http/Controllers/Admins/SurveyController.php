03ecfb28b78f2e1d2943d623622d27b0bd4bbc8f
Mike Lovely <mike-lovely@hotmail.co.uk>
2016-09-17 23:36:25 +0100

    remove unused surveys methods

--- app/Http/Controllers/Admins/SurveyController.php
+++ app/Http/Controllers/Admins/SurveyController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers\Admins;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SurveyForm;
 use App\Models\Survey;
