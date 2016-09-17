398dc1be398c27e7fc29bd7dfc09ab651b5e6591
Mike Lovely <mike-lovely@hotmail.co.uk>
2016-09-17 23:44:44 +0100

    add basic groups views

---
+++ resources/views/admins/surveys/groups/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Create a new group</h3>
+            </div>
+            <div class="panel-body">
+                @include('admins.surveys.groups.form', [
+                    'url' => route('surveys.groups.store', [$survey->id, $group->id]),
+                    'method' => 'post',
+                    'button' => 'Create',
+                ])
+            </div>
+        </div>
+    </div>
+@endsection
