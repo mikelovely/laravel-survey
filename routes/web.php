f64682338397f182c12d7a6a436d182783921763
Mike Lovely <mike-lovely@hotmail.co.uk>
2017-02-27 12:38:43 +0000

    Add Manager role Routes

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,15 @@ Route::get('/home', 'HomeController@index');
 
 Route::group(['prefix' => 'admins', 'middleware' => ['web', 'auth', 'role:admin']], function () {
     Route::resource('surveys.groups.questions.answers', 'Admins\AnswerController');
-    Route::resource('surveys.groups.questions', 'Admins\QuestionController');
-    Route::resource('surveys.groups', 'Admins\GroupController');
+    Route::resource('surveys.groups.questions', 'Admins\QuestionController', ['except' => ['index']]);
+    Route::resource('surveys.groups', 'Admins\GroupController', ['except' => ['index']]);
     Route::resource('surveys', 'Admins\SurveyController');
 });
+
+Route::group(['middleware' => ['web', 'auth', 'role:manager']], function () {
+    Route::resource('surveys.groups.questions.answers', 'Manager\AnswerController');
+    Route::resource('surveys.groups.questions', 'Manager\QuestionController', ['except' => ['index']]);
+    Route::resource('surveys.groups', 'Manager\GroupController', ['except' => ['index']]);
+    Route::resource('surveys', 'Manager\SurveyController');
+});
+
